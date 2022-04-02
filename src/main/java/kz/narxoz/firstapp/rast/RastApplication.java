ad31797b861da181728578629cce8752ac383957
abylai <abok_003@mail.ru>
2022-04-02 10:06:49 +0600

    first commit

---
+++ src/main/java/kz/narxoz/firstapp/rast/RastApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.firstapp.rast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/narxoz/firstapp/rast/RastApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.firstapp.rast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
